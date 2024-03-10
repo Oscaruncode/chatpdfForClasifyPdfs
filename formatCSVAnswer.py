3a41c4d31a2533e00ca01446d0be110a11232b96
oscaruncode <oscarfuentesperezosp@gmail.com>
2024-03-09 19:56:18 -0500

    Pending solve jump lines in csv for IA answer

--- formatCSVAnswer.py
+++ formatCSVAnswer.py
@@ -1,5 +1,7 @@
 import re
 def formatCSVanswer(text):
+    print(text)
     modified_text = text.replace(",", "_")
     modified_text = re.sub(r'(?<!^)(\d+)\?', r',', modified_text)
+    print(modified_text)
     return modified_text[2:]
